8d31fb3411ebe1fad5063d2f08fd49eb8d6d357a
WIlliam <williamccondori@outlook.com>
2017-09-02 21:01:11 -0500

    Término del paso 2 del tutorial de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,28 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   Titulo = 'Lista de héroes';
-  Heroe: Heroe= {
-    Id : 1,
-    Nombre : 'Superman'
-  };
+  Heroes = HEROES;
+  HeroeSeleccionado: Heroe;
+
+  onSelect(heroe: Heroe): void {
+    this.HeroeSeleccionado = heroe;
+  }
 }
+
+
+
+const HEROES: Heroe[] = [
+  { Id: 11, Nombre: 'Mr. Nice' },
+  { Id: 12, Nombre: 'Narco' },
+  { Id: 13, Nombre: 'Bombasto' },
+  { Id: 14, Nombre: 'Celeritas' },
+  { Id: 15, Nombre: 'Magneta' },
+  { Id: 16, Nombre: 'RubberMan' },
+  { Id: 17, Nombre: 'Dynama' },
+  { Id: 18, Nombre: 'Dr IQ' },
+  { Id: 19, Nombre: 'Magma' },
+  { Id: 20, Nombre: 'Tornado' }
+];
 
 export class Heroe {
   Id : number;
