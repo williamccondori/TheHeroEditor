573d4ce29d9b3ffc5ba3133ee5aaf97bf0581241
WIlliam <williamccondori@outlook.com>
2017-09-02 20:37:46 -0500

    Creación del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  Titulo = 'Lista de héroes';
+  Heroe: Heroe= {
+    Id : 1,
+    Nombre : 'Superman'
+  };
+}
+
+export class Heroe {
+  Id : number;
+  Nombre: string;
 }
