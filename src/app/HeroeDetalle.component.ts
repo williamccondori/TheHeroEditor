c946ee50624d9fd200226ca03afa5b9317f17c8c
WIlliam <williamccondori@outlook.com>
2017-09-02 23:02:52 -0500

    Comentarios útiles

--- src/app/HeroeDetalle.component.ts
+++ src/app/HeroeDetalle.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
-
 import { Heroe } from '../models/Heroe';
 
+/**
+ * Elementos del componente
+ */
 @Component({
   selector: 'HeroeDetalle',
   templateUrl: './HeroeDetalle.component.html'
 })
 
+/**
+ * Clase pública del componente.
+ */
 export class HeroeDetalleComponent {
   @Input() Heroe: Heroe;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-
 import { Heroe } from '../models/Heroe';
 
+/**
+ * Array de elementos de tipo Heroe, definidas en una constante.
+ */
 const HEROES: Heroe[] = [
   { Id: 11, Nombre: 'Mr. Nice' },
   { Id: 12, Nombre: 'Narco' },
@@ -15,17 +17,28 @@ const HEROES: Heroe[] = [
   { Id: 20, Nombre: 'Tornado' }
 ];
 
+/**
+ * Elementos del componente.
+ */
 @Component({
-  selector: 'app-root',
+  selector: 'Aplicacion',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
+/**
+ * Clase pública del componente.
+ */
 export class AppComponent {
-  Titulo = 'Lista de héroes';
-  Heroes = HEROES;
-  HeroeSeleccionado: Heroe;
+  
+  Titulo = 'Lista de héroes'; // Variable que almacena el tíulo de la página.
+  Heroes = HEROES; // Lista de héroes.
+  HeroeSeleccionado: Heroe; // Héroe seleccionado.
 
+  /**
+   * Obtiene los datos del héroe seleccionado y lo asigna a una variable.
+   * @param heroe Héroe seleccionado
+   */
   Mostrar(heroe: Heroe): void {
     this.HeroeSeleccionado = heroe;
   }
